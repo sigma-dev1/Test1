9c91c6220ada14215116c6eabbba03b8ec9642c1
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-12-09 22:25:35 +0100

    Update add.py

--- add.py
+++ add.py
@@ -3,8 +3,8 @@ import pickle
 import os
 
 # API credentials
-api_id = '21963510'
-api_hash = 'eddfccf6e4ea21255498028e5af25eb1'
+api_id = '25373607'
+api_hash = '3b559c2461a210c9654399b66125bc0b'
 
 # Directory per le sessioni
 sessions_dir = 'sessions'
